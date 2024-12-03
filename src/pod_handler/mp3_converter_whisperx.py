a56bcd863ca2d7c31c6c567a03aed19c9c6cff4e
jon fox <jon@foxsolutions.dev>
2024-12-02 21:19:16 -0600

    logs for original duration

--- src/pod_handler/mp3_converter_whisperx.py
+++ src/pod_handler/mp3_converter_whisperx.py
@@ -372,6 +372,7 @@ def remove_ads_from_audio(audio_file):
     # Save the result
     finished_audio_without_ads.export("finished_audio_without_ads.mp3", format="mp3")
     # Return the duration of the finished audio in seconds
+    logger.info(f"Audio with ads duration: {original_duration} seconds")
     logger.info(f"Finished audio without ads duration: {len(finished_audio_without_ads) / 1000} seconds")
     return len(finished_audio_without_ads) / 1000, original_duration
 
