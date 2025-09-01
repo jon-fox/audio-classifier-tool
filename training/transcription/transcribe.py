eb6ac0c0c074d7fed5093d6877f39753a145647f
jon fox <jon@foxsolutions.dev>
2025-09-01 01:30:40 -0500

    uv migration, transcribe changes

--- training/transcription/transcribe.py
+++ training/transcription/transcribe.py
@@ -565,27 +565,6 @@ def process_audio_segment(index, audio_segment, total_segments):
             f"Before entering the lock for index {index}: {threading.get_ident()}"
         )
 
-        with lock:
-            logger.info(
-                f"Thread {threading.get_ident()} is entering the openai api call for file transcript_{index}_logging.json"
-            )
-            run, thread = get_specific_timestamps_using_llm(
-                f"transcript_{index}_logging.json",
-                os.path.join(script_dir, f"transcript_{index}_logging.json"),
-                lock,
-            )
-            logger.info(
-                f"Put prompts into assistant thread openai thread {thread.id} and polling run {run.id}"
-            )
-            min_ms, max_ms, confidence_score = get_run_output(run=run, thread=thread)
-            logger.info(
-                f"Thread {threading.get_ident()} has exited the openai api call for file transcript_{index}_logging.json"
-            )
-            logger.info(
-                f"OpenAI Thread {thread.id} and Run {run.id}:: Finished with values MIN[{min_ms}], MAX[{max_ms}], "
-                f"and Confidence Score [{confidence_score}], transcript_{index}_logging.json"
-            )
-
         if min_ms == float("inf") and max_ms == float("-inf"):
             logger.info(
                 f"No ads found in the segment for transcript transcript_{index}_logging.json"
@@ -721,5 +700,5 @@ def remove_ads_from_audio(audio_file, podcast_description):
 
 podcast_description = """<p>Today we’ll hear about: </p><ul>\n<li>A young owner looking for help establishing processes in his fast- growing business </li>\n<li>A woman looking to fire an employee who won’t see it coming </li>\n<li>Dave Ramsey’s take on Home Depot requiring corporate employees to work in retail stores </li>\n<li>A business owner looking for advice on profit sharing with her team </li>\n</ul><p> </p><p><strong>Next Steps</strong> </p><ul>\n<li>📞 Have a question for the show? Call 844-944-1070 or send us a message: <a href=\"https://ter.li/ask-us\">https://ter.li/ask-us</a> </li>\n<li>📚 Learn about the EntreLeadership System: <a href=\"https://ter.li/system-p\">https://ter.li/system-p</a> </li>\n<li>💻 Get EntreLeadership Elite for your business: <a href=\"https://ter.li/elite-p\">https://ter.li/elite-p</a> </li>\n<li>✉️ Sign up to receive tactical tools, advice and resources in your inbox every week: <a href=\"https://ter.li/enl\">https://ter.li/enl</a> </li>\n<li>🏢 Attend EntreLeadership Summit: <a href=\"https://ter.li/summit\">https://ter.li/summit</a>  </li>\n<li>🎤 Attend EntreLeadership Master Series: <a href=\"https://ter.li/masterseries\">https://ter.li/masterseries</a>  </li>\n</ul><p> </p><p><strong>Offers From Today's Sponsors</strong> </p><ul>\n<li>💼 Go to<a href=\"https://www.belaysolutions.com/Entreleadership\"> <strong>Belay Solutions</strong></a> or text ENTRE to 55123 for their free resource! </li>\n<li>💻 Visit<a href=\"https://www.netsuite.com/Ramsey\"> <strong>NetSuite</strong></a> today to learn more </li>\n<li>🧾 Visit<a href=\"https://www.payority.com/entreleadership\"> </a><a href=\"https://www.payority.com/entreleadership\"><strong>Payority</strong></a> for a free consultation! </li>\n<li>📝 Use code entre15 to get 15% off your first year of <a href=\"https://www.trainual.com/entre\"><strong>Trainual</strong></a> </li>\n</ul><p> </p><p><strong>Listen to More From Ramsey Network</strong> </p><p>🎙️ <a href=\"https://ter.li/gpny1a\">The Ramsey Show</a> </p><p>💸 <a href=\"https://ter.li/430qk2\">The Ramsey Show Highlights</a> </p><p><strong>🧠</strong> <a href=\"https://ter.li/w9syza\">The Dr. John Delony Show</a> </p><p>🍸 <a href=\"https://ter.li/k3waa1\">Smart Money Happy Hour</a> </p><p>💡 <a href=\"https://ter.li/j3ahu7\">The Rachel Cruze Show</a> </p><p>💰 <a href=\"https://ter.li/99j2kb\">George Kamel</a> </p><p>💼 <a href=\"https://ter.li/puh2qh\">The Ken Coleman Show</a> </p><p> </p><p><a href=\"https://www.megaphone.fm/adchoices\">Learn More About Your Ad Choices</a>  </p><p><a href=\"https://www.ramseysolutions.com/company/policies/privacy-policy\">Ramsey Solutions Privacy Policy</a> </p>"""
 
-# if __name__ == "__main__":
-#     remove_ads_from_audio("downloads/potp1201art19.mp3", podcast_description)
+if __name__ == "__main__":
+    remove_ads_from_audio("downloads/potp1201art19.mp3", podcast_description)
